d9792637cd5b1aea5a43e12cf41ff8ec2684c632
MhuhammadAbdullah <abaloch0033@gmail.com>
2024-08-23 11:54:13 +0500

    updated

---
+++ app.js
@@ -0,0 +1,25 @@
+// Assignment : 01
+// Question : 01
+
+// alert("Hello World!");
+
+// Question : 02
+
+// alert("Erro! Please enter a valid password");
+
+// Question : 03
+
+// alert("Welcome to JS Land... \n Happy Coding")
+
+// Question : 04
+
+// alert("Welcome to JS Land...")
+// alert("Prevent this page from creating additional dialogs.")
+
+// Question : 05
+
+// alert("Hello...I can run JS through my web browser's console")
+
+
+// Assignment : 02
+// Question : 01
